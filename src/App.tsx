80a77642920d14c8a21691d8597a55ca9ce7fb09
Nikita <tr_nikita@mail.ru>
2023-02-02 00:13:52 +0700

    add themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import React, {Suspense} from 'react';
+import './styles/index.scss'
 import {Link, Route, Routes} from "react-router-dom";
-import './index.scss'
 import {MainPageAsync} from "./pages/MainPage/MainPage/MainPage.async";
 import {AboutPageAsync} from "./pages/MainPage/AboutPage/AboutPage.async";
+import {useTheme} from "./theme/useTheme";
 
 const App = () => {
+
+    const {theme, toggleTheme} = useTheme()
+
     return (
-        <div className='app'>
+        <div className={`app ${theme}`}>
+            <button onClick={toggleTheme}>TOGGLE</button>
             <Link to={'/'}>Главная</Link>
             <Link to={'/about'}>О сайте</Link>
             <Suspense fallback={<div>Загрузка...</div>}>
