b2cc02a7e4316c51bb1cb47c022dc81154a98a4f
Nikita <tr_nikita@mail.ru>
2023-02-08 00:54:44 +0700

    add Sidebar

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,17 +1,40 @@
-import React from 'react';
+import React, {Suspense} from 'react';
 import './styles/index.scss'
 import {classNames} from "shared/lib/classNames/classNames";
 import {useTheme} from "app/providers/ThemeProvider";
 import {AppRouter} from "app/providers/router";
 import {Navbar} from "widgets/Navbar";
+import {Sidebar} from "widgets/Sidebar";
+import {useTranslation} from "react-i18next";
+
+const Component = () => {
+    const {t, i18n} = useTranslation();
+
+    const toggle = async () => {
+        i18n.changeLanguage(i18n.language === 'ru' ? 'en' : 'ru');
+    }
+
+    return (
+        <div>
+            <button onClick={toggle}>{t("Перевод")}</button>
+            {t('Тестовый пример')}
+        </div>
+    )
+}
 
 const App = () => {
     const {theme} = useTheme()
 
     return (
-        <div className={classNames('app', {hovered: true, selected: true}, [theme, 'cls2', 'cls3'])}>
-            <Navbar/>
-            <AppRouter/>
+        <div className={classNames('app', {}, [theme])}>
+            <Suspense fallback="">
+                <Navbar/>
+                <Component/>
+                <div className='content-page'>
+                    <Sidebar/>
+                    <AppRouter/>
+                </div>
+            </Suspense>
         </div>
     );
 };
