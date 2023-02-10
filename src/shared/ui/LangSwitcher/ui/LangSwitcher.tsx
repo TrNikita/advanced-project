f5aef3294236124b7edb449241831ad657bcfe93
Nikita <tr_nikita@mail.ru>
2023-02-11 03:25:41 +0700

    fix files with stylelint

--- src/shared/ui/LangSwitcher/ui/LangSwitcher.tsx
+++ src/shared/ui/LangSwitcher/ui/LangSwitcher.tsx
@@ -1,5 +1,4 @@
 import { classNames } from 'shared/lib/classNames/classNames';
-import cls from './LangSwitcher.module.scss';
 import { useTranslation } from 'react-i18next';
 import React from 'react';
 import { Button, ThemeButton } from 'shared/ui/Button/Button';
@@ -12,11 +11,11 @@ export const LangSwitcher = ({ className }: LangSwitcherProps) => {
 	const { t, i18n } = useTranslation();
 
 	const toggle = async () => {
-		i18n.changeLanguage(i18n.language === 'ru' ? 'en' : 'ru');
+		await i18n.changeLanguage(i18n.language === 'ru' ? 'en' : 'ru');
 	};
 	return (
 		<Button
-			className={classNames(cls.LangSwitcher, {}, [className])}
+			className={classNames('', {}, [className])}
 			theme={ThemeButton.CLEAR}
 			onClick={toggle}
 		>
--- src/shared/ui/ThemeSwitcher/ui/ThemeSwitcher.tsx
+++ src/shared/ui/ThemeSwitcher/ui/ThemeSwitcher.tsx
@@ -1,5 +1,4 @@
 import { classNames } from 'shared/lib/classNames/classNames';
-import cls from './ThemeSwitcher.module.scss';
 import React from 'react';
 import { Theme, useTheme } from 'app/providers/ThemeProvider';
 import LightIcon from 'shared/assets/icons/theme-light.svg';
@@ -14,7 +13,9 @@ export const ThemeSwitcher = ({ className }: ThemeSwitcherProps) => {
 	const { theme, toggleTheme } = useTheme();
 
 	return (
-		<Button theme={ThemeButton.CLEAR} className={classNames(cls.ThemeSwitcher, {}, [className])} onClick={toggleTheme}
+		<Button
+			theme={ThemeButton.CLEAR}
+			className={classNames('', {}, [className])} onClick={toggleTheme}
 		>
 			{theme === Theme.DARK ? <DarkIcon/> : <LightIcon/>}
 		</Button>
