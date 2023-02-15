07989ebd046a717c395e7e46d10b0e422ba5e106
Nikita <tr_nikita@mail.ru>
2023-02-16 00:00:12 +0700

    fix actions

--- src/shared/config/storybook/ThemeDecorator/ThemeDecorator.tsx
+++ src/shared/config/storybook/ThemeDecorator/ThemeDecorator.tsx
@@ -1,10 +1,9 @@
 import { Story } from '@storybook/react';
 import { Theme } from 'app/providers/ThemeProvider';
 
-export const ThemeDecorator = (theme: Theme) => (StoryComponent: Story) => {
-	return (
-		<div className={`app ${theme}`}>
-			<StoryComponent/>
-		</div>
-	);
-};
+// eslint-disable-next-line react/display-name
+export const ThemeDecorator = (theme: Theme) => (StoryComponent: Story) => (
+	<div className={`app ${theme}`}>
+		<StoryComponent/>
+	</div>
+);
