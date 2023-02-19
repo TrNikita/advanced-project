0149bdb85bea3949b7f222eea951c43820b43970
Nikita <tr_nikita@mail.ru>
2023-02-19 23:56:48 +0700

    add Modal

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,18 +1,33 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
 import cls from './Navbar.module.scss';
-import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
 import { useTranslation } from 'react-i18next';
+import { Modal } from 'shared/ui/Modal/Modal';
+import { Button, ButtonTheme } from 'shared/ui/Button/Button';
 
 interface NavbarProps {
-    className?: string;
+	className?: string;
 }
 
 export const Navbar = ({ className }: NavbarProps) => {
 	const { t } = useTranslation();
+	const [isAuthModal, setIsAuthModal] = useState(false);
+
+	const onToggleModal = useCallback(() => {
+		setIsAuthModal(prevState => !prevState);
+	}, []);
+
 	return (
 		<div className={classNames(cls.Navbar, {}, [className])}>
-			<div className={cls.links}>/</div>
+			<Button
+				theme={ButtonTheme.CLEAR}
+				className={cls.links}
+				onClick={onToggleModal}
+			>
+				{t('Войти')}
+			</Button>
+			<Modal isOpen={isAuthModal} onClose={onToggleModal}>
+			</Modal>
 		</div>
 	);
 };
