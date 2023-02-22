0b4a3a28df58822d03fa11576a6613f964cc03aa
Nikita <tr_nikita@mail.ru>
2023-02-23 02:37:37 +0700

    add input

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
+import { Input } from 'shared/ui/Input/Input';
 
 const MainPage = () => {
 	const { t } = useTranslation('main');
+	const [value, setValue] = useState('');
+
+	const onChange = (val: string) => {
+		setValue(val);
+	};
+
 	return (
 		<div>
 			{t('Главная страница')}
+			<Input
+				placeholder={t('Введите текст')}
+				onChange={onChange}
+				value={value}
+			/>
 		</div>
 	);
 };
