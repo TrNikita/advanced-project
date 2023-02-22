c49ebc4aa23eea88a4b552f1360bd60d5b0895e6
Nikita <tr_nikita@mail.ru>
2023-02-23 02:49:43 +0700

    add stories Input, LoginForm.

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,23 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useTranslation } from 'react-i18next';
-import { Input } from 'shared/ui/Input/Input';
 
 const MainPage = () => {
 	const { t } = useTranslation('main');
-	const [value, setValue] = useState('');
-
-	const onChange = (val: string) => {
-		setValue(val);
-	};
 
 	return (
 		<div>
 			{t('Главная страница')}
-			<Input
-				placeholder={t('Введите текст')}
-				onChange={onChange}
-				value={value}
-			/>
 		</div>
 	);
 };
