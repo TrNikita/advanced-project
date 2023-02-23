84aa45b9e6b8bec3b3172668c740841321870e36
Nikita <tr_nikita@mail.ru>
2023-02-24 02:53:34 +0700

    add login by username

--- src/features/AuthByUsername/ui/LoginForm/LoginForm.stories.tsx
+++ src/features/AuthByUsername/ui/LoginForm/LoginForm.stories.tsx
@@ -14,6 +14,6 @@ export default {
 
 const Template: ComponentStory<typeof LoginForm> = (args) => <LoginForm {...args} />;
 
-export const Primary = Template.bind({});
-Primary.args = {
-};
+// export const Primary = Template.bind({});
+// Primary.args = {
+// };
--- src/features/AuthByUsername/ui/LoginForm/LoginForm.tsx
+++ src/features/AuthByUsername/ui/LoginForm/LoginForm.tsx
@@ -1,18 +1,39 @@
 import { classNames } from 'shared/lib/classNames/classNames';
 import cls from './LoginForm.module.scss';
 import { useTranslation } from 'react-i18next';
-import { Button } from 'shared/ui/Button/Button';
+import { Button, ButtonTheme } from 'shared/ui/Button/Button';
 import { Input } from 'shared/ui/Input/Input';
+import { useDispatch, useSelector } from 'react-redux';
+import { memo, useCallback } from 'react';
+import { loginActions } from '../../model/slice/loginSlice';
+import { getLoginState } from '../../model/selectors/getLoginState/getLoginState';
+import { loginByUsername } from '../../model/services/loginByUsername/loginByUsername';
+import { Text, TextTheme } from 'shared/ui/Text/Text';
 
 interface LoginFormProps {
 	className?: string;
 }
 
-export const LoginForm = (props: LoginFormProps) => {
+export const LoginForm = memo((props: LoginFormProps) => {
 	const { t } = useTranslation();
 	const {
 		className,
 	} = props;
+
+	const dispatch = useDispatch();
+	const { username, password, isLoading, error } = useSelector(getLoginState);
+
+	const onChangeUsername = useCallback((value: string) => {
+		dispatch(loginActions.setUsername(value));
+	}, [dispatch]);
+
+	const onChangePassword = useCallback((value: string) => {
+		dispatch(loginActions.setPassword(value));
+	}, [dispatch]);
+
+	const onLoginClick = useCallback(() => {
+		dispatch(loginByUsername({ username, password }));
+	}, [dispatch, password, username]);
 
 	return (
 		<div className={
@@ -22,18 +43,31 @@ export const LoginForm = (props: LoginFormProps) => {
 				[className]
 			)}
 		>
+			<Text title={t('Форма авторизации')}/>
+			{error && <Text text={error} theme={TextTheme.ERROR}/>}
 			<Input
 				autoFocus
 				type='text'
 				className={cls.input}
-				placeholder={t('введите username')}/>
+				placeholder={t('введите username')}
+				onChange={onChangeUsername}
+				value={username}
+			/>
 			<Input
 				type='text'
 				className={cls.input}
-				placeholder={t('введите пароль')}/>
-			<Button className={cls.loginBtn}>
+				placeholder={t('введите пароль')}
+				onChange={onChangePassword}
+				value={password}
+			/>
+			<Button
+				theme={ButtonTheme.OUTLINE}
+				className={cls.loginBtn}
+				onClick={onLoginClick}
+				disabled={isLoading}
+			>
 				{t('Войти')}
 			</Button>
 		</div>
 	);
-};
+});
