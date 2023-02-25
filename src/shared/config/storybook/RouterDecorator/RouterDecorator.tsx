65880c154d161f00f9dddd2ac6e156efa5a187b5
Nikita <tr_nikita@mail.ru>
2023-02-25 22:51:10 +0700

    add storybook decorator

--- src/shared/config/storybook/RouterDecorator/RouterDecorator.tsx
+++ src/shared/config/storybook/RouterDecorator/RouterDecorator.tsx
@@ -1,11 +1,14 @@
 import { Story } from '@storybook/react';
+import { Suspense } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
 export const RouterDecorator = (story: () => Story) => {
 	return (
-		<BrowserRouter>
-			{story()}
-		</BrowserRouter>
+		<Suspense fallback=''>
+			<BrowserRouter>
+				{story()}
+			</BrowserRouter>
+		</Suspense>
 	);
 };
 
