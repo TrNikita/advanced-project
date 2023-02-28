40b1c4ad13f69613bcd61409fa2b8ce719198d08
Nikita <tr_nikita@mail.ru>
2023-02-28 23:53:14 +0700

    add tests

---
+++ src/features/AuthByUsername/model/services/loginByUsername/loginByUsername.test.ts
@@ -0,0 +1,32 @@
+import { Dispatch } from '@reduxjs/toolkit';
+import axios from 'axios';
+import { loginByUsername } from 'features/AuthByUsername/model/services/loginByUsername/loginByUsername';
+import { StateSchema } from 'app/providers/StoreProvider';
+import { userActions } from 'entities/User';
+
+jest.mock('axios');
+
+const mockedAxios = jest.mocked(axios, true);
+
+describe('loginByUsername.test', () => {
+	let dispatch: Dispatch;
+	let getState: () => StateSchema;
+
+	beforeEach(() => {
+		dispatch = jest.fn();
+		getState = jest.fn();
+	});
+
+	test('', async () => {
+		const userValue = { username: '123', id: '1' };
+		mockedAxios.post.mockReturnValue(Promise.resolve({ data: userValue }));
+		const action = loginByUsername({ username: '123', password: '123' });
+		const result = await action(dispatch, getState, undefined);
+		console.log('result', result);
+
+		expect(dispatch).toHaveBeenCalledWith(userActions.setAuthData(userValue));
+		expect(mockedAxios.post).toHaveBeenCalled();
+		expect(result.meta.requestStatus).toBe('fulfilled');
+	});
+});
+
