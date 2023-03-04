31c058a61152448fb80a01b310c214a1c6293d9b
Nikita <tr_nikita@mail.ru>
2023-03-04 18:16:40 +0700

    add fetchProfileData

---
+++ src/entities/Profile/model/services/fetchProfileData/fetchProfileData.ts
@@ -0,0 +1,22 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import { ThunkConfig } from 'app/providers/StoreProvider';
+import { Profile } from '../../types/profile';
+
+export const fetchProfileData =
+	createAsyncThunk<Profile, void, ThunkConfig<string>>(
+		'login/loginByUsername',
+		async (_, thunkApi) => {
+			const { extra, rejectWithValue } = thunkApi;
+
+			try {
+				const response = await extra.api.get<Profile>('/profile');
+
+				return response.data;
+			} catch (e) {
+				console.log('e', e);
+				return rejectWithValue(
+					'Error'
+				);
+			}
+		}
+	);
