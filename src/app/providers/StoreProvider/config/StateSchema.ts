22d0e66dff1ebc37847abebdcd9697ba86b0170b
Nikita <tr_nikita@mail.ru>
2023-03-14 00:23:00 +0700

    add Article Schema, Slices

--- src/app/providers/StoreProvider/config/StateSchema.ts
+++ src/app/providers/StoreProvider/config/StateSchema.ts
@@ -2,6 +2,8 @@ import { CounterSchema } from 'entities/Counter';
 import { UserSchema } from 'entities/User';
 import { LoginSchema } from 'features/AuthByUsername';
 import { ProfileSchema } from 'entities/Profile';
+import { ArticleDetailsSchema } from 'entities/Article';
+
 import {
 	AnyAction, CombinedState, Dispatch, EnhancedStore, Reducer, ReducersMapObject
 } from '@reduxjs/toolkit';
@@ -16,6 +18,7 @@ export interface StateSchema {
 	//Асинхронные редюсеры
 	loginForm?: LoginSchema;
 	profile?: ProfileSchema;
+	articleDetails?: ArticleDetailsSchema;
 }
 
 export type StateSchemaKey = keyof StateSchema;
--- src/entities/Article/ui/ArticleDetails/ArticleDetails.tsx
+++ src/entities/Article/ui/ArticleDetails/ArticleDetails.tsx
@@ -1,18 +1,29 @@
 import { classNames } from 'shared/lib/classNames/classNames';
 import cls from './ArticleDetails.module.scss';
 import { useTranslation } from 'react-i18next';
+import {
+	DynamicModuleLoader,
+	ReducersList
+} from 'shared/lib/components/DynamicModuleLoader/DynamicModuleLoader';
+import { articleDetailsReducer } from '../../model/slice/articleDetailsSlice';
 
 interface ArticleDetailsProps {
 	className?: string;
 }
+
+const reducers: ReducersList = {
+	articleDetails: articleDetailsReducer
+};
 
 export const ArticleDetails = (props: ArticleDetailsProps) => {
 	const { t } = useTranslation();
 	const { className } = props;
 
 	return (
-		<div className={classNames(cls.ArticleDetails, {}, [className])}>
-			Article Details
-		</div>
+		<DynamicModuleLoader reducers={reducers} removeAfterUnmount={true}>
+			<div className={classNames(cls.ArticleDetails, {}, [className])}>
+				Article Details
+			</div>
+		</DynamicModuleLoader>
 	);
 };
