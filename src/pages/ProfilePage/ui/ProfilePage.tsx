4d9a554aad136e9b121b4de7e4eb653ac98328ca
Nikita <tr_nikita@mail.ru>
2023-03-19 23:14:28 +0700

    add fetch profile by Id

--- src/pages/ProfilePage/ui/ProfilePage.tsx
+++ src/pages/ProfilePage/ui/ProfilePage.tsx
@@ -1,9 +1,8 @@
 import { classNames } from 'shared/lib/classNames/classNames';
 import {
-	DynamicModuleLoader,
-	ReducersList
+	DynamicModuleLoader, ReducersList
 } from 'shared/lib/components/DynamicModuleLoader/DynamicModuleLoader';
-import { useCallback, useEffect } from 'react';
+import { useCallback } from 'react';
 import { useSelector } from 'react-redux';
 import { useAppDispatch } from 'shared/lib/hooks/useAppDispatch/useAppDispatch';
 import {
@@ -15,13 +14,16 @@ import {
 	getProfileValidateErrors,
 	profileActions,
 	ProfileCard,
-	profileReducer, ValidateProfileErrors
+	profileReducer,
+	ValidateProfileErrors
 } from 'entities/Profile';
 import { ProfilePageHeader } from './ProfilePageHeader/ProfilePageHeader';
 import { Currency } from 'entities/Currency';
 import { Country } from 'entities/Country';
 import { Text, TextTheme } from 'shared/ui/Text/Text';
 import { useTranslation } from 'react-i18next';
+import { useInitialEffect } from 'shared/lib/hooks/useInitialEffect/useInitialEffect';
+import { useParams } from 'react-router-dom';
 
 const reducers: ReducersList = {
 	profile: profileReducer
@@ -44,6 +46,7 @@ const ProfilePage = (props: ProfilePageProps) => {
 	const error = useSelector(getProfileError);
 	const readonly = useSelector(getProfileReadonly);
 	const validateErrors = useSelector(getProfileValidateErrors);
+	const { id } = useParams<{ id: string }>();
 
 	const validateErrorTranslates = {
 		[ValidateProfileErrors.SERVER_ERROR]: t('Ошибка сервера'),
@@ -53,11 +56,11 @@ const ProfilePage = (props: ProfilePageProps) => {
 		[ValidateProfileErrors.INCORRECT_COUNTRY]: t('Некорректный регион')
 	};
 
-	useEffect(() => {
-		if (__PROJECT__ !== 'storybook') {
-			dispatch(fetchProfileData());
+	useInitialEffect(() => {
+		if (id) {
+			dispatch(fetchProfileData(id));
 		}
-	}, [dispatch]);
+	});
 
 	const onChangeFirstname = useCallback((value?: string) => {
 		dispatch(profileActions.updateProfile({ first: value || '' })
