63b6d19c028aabb430ae06f0c616c8f2ddcabc41
Nikita <tr_nikita@mail.ru>
2023-04-11 00:02:25 +0700

    add rtkApi

--- src/app/providers/StoreProvider/config/store.ts
+++ src/app/providers/StoreProvider/config/store.ts
@@ -5,6 +5,7 @@ import { userReducer } from 'entities/User';
 import { createReducerManager } from 'app/providers/StoreProvider/config/reducerManager';
 import { $api } from 'shared/api/api';
 import { uiReducer } from 'features/UI';
+import { rtkApi } from 'shared/api/rtkApi';
 
 export function createReduxStore(
 	initialState?: StateSchema,
@@ -15,6 +16,7 @@ export function createReduxStore(
 		counter: counterReducer,
 		user: userReducer,
 		ui: uiReducer,
+		[rtkApi.reducerPath]: rtkApi.reducer,
 	};
 
 	const reducerManager = createReducerManager(rootReducers);
@@ -30,8 +32,8 @@ export function createReduxStore(
 		middleware: getDefaultMiddleware => getDefaultMiddleware({
 			thunk: {
 				extraArgument: extraArg
-			}
-		})
+			},
+		}).concat(rtkApi.middleware)
 	});
 
 	// eslint-disable-next-line @typescript-eslint/ban-ts-comment
