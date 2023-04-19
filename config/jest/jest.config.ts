9f8cee48a49b7a93d7999888614987ff4ea1fa04
Nikita <tr_nikita@mail.ru>
2023-04-20 01:02:56 +0700

    add script to add Aliases // fix tests

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -54,6 +54,7 @@ export default {
 	moduleNameMapper: {
 		'\\.s?css$': 'identity-obj-proxy',
 		'\\.svg': path.resolve(__dirname, 'jestEmptyComponent.tsx'),
+		'^@/(.*)$': '<rootDir>/src/$1',
 	},
 
 	// Use this configuration option to add custom reporters to Jest
