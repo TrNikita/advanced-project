9490c78a65a943215bc962cb884d86693b705462
Nikita <tr_nikita@mail.ru>
2023-04-19 23:13:58 +0700

    add script to add Aliases

--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -14,6 +14,7 @@ export default ({ config }: { config: webpack.Configuration }) => {
 	};
 	config!.resolve!.modules!.push(paths.src);
 	config!.resolve!.extensions!.push('.ts', '.tsx');
+	config!.resolve!.alias = { '@': path.resolve(__dirname, '..', '..', 'src') };
 
 	const rules = config.module!.rules as RuleSetRule[];
 
