3f047823a3240de43db584898b48153c3fbf38a6
Nikita <tr_nikita@mail.ru>
2023-04-21 01:55:00 +0700

    add ArticleRatingApi

---
+++ src/features/articleRating/ui/ArticleRating/ArticleRating.tsx
@@ -0,0 +1,69 @@
+import { memo, useCallback } from 'react';
+import { useTranslation } from 'react-i18next';
+import { useSelector } from 'react-redux';
+
+import { RatingCard } from '@/entities/Rating';
+import { getUserAuthData } from '@/entities/User';
+import { Skeleton } from '@/shared/ui/Skeleton/Skeleton';
+
+import { useGetArticleRating, useRateArticle } from '../../api/articleRatingApi';
+
+
+export interface ArticleRatingProps {
+	className?: string;
+	articleId: string;
+}
+
+const ArticleRating = memo((props: ArticleRatingProps) => {
+	const { t } = useTranslation();
+	const { className, articleId } = props;
+	const userData = useSelector(getUserAuthData);
+
+	const { data, isLoading } = useGetArticleRating({
+		articleId,
+		userId: userData?.id ?? ''
+	});
+
+	const [rateArticleMutation] = useRateArticle();
+
+	const handleRateArticle = useCallback((startsCount: number, feedback?: string) => {
+		try {
+			rateArticleMutation({
+				userId: userData?.id ?? '',
+				articleId,
+				rate: startsCount,
+				feedback
+			});
+		} catch (e) {
+			console.log('error', e);
+		}
+	}, [articleId, rateArticleMutation, userData?.id]);
+
+	const onAccept = useCallback((startsCount: number, feedback?: string) => {
+		handleRateArticle(startsCount, feedback);
+	}, [handleRateArticle]);
+
+	const onCancel = useCallback((startsCount: number) => {
+		handleRateArticle(startsCount);
+	}, [handleRateArticle]);
+
+	if (isLoading) {
+		return <Skeleton width='100%' height={120}/>;
+	}
+
+	const rating = data?.[0];
+
+	return (
+		<RatingCard
+			onCancel={onCancel}
+			onAccept={onAccept}
+			rate={rating?.rate}
+			className={className}
+			title={t('Оцените статью')}
+			feedbackTitle={t('Оставьте свой отзыв о статье, это поможет улучшить качество')}
+			hasFeedback
+		/>
+	);
+});
+
+export default ArticleRating;
--- src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.tsx
+++ src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.tsx
@@ -2,6 +2,7 @@ import { memo } from 'react';
 import { useParams } from 'react-router-dom';
 
 import { ArticleDetails } from '@/entities/Article';
+import { ArticleRating } from '@/features/articleRating';
 import { ArticleRecommendationsList } from '@/features/articleRecommendationsList';
 import { classNames } from '@/shared/lib/classNames/classNames';
 import { DynamicModuleLoader, ReducersList } from '@/shared/lib/components/DynamicModuleLoader/DynamicModuleLoader';
@@ -28,13 +29,18 @@ const ArticleDetailsPage = (props: ArticleDetailsPageProps) => {
 	const { className } = props;
 	const { id } = useParams<{ id: string }>();
 
+	if (!id) {
+		return null;
+	}
+
 	return (
 		<DynamicModuleLoader reducers={reducers} removeAfterUnmount>
 			<Page className={classNames(cls.ArticleDetailsPage, {}, [className])}>
-				<VStack gap="16" max>
-					<ArticleDetailsPageHeader />
-					<ArticleDetails id={id} />
-					<ArticleRecommendationsList />
+				<VStack gap='16' max>
+					<ArticleDetailsPageHeader/>
+					<ArticleDetails id={id}/>
+					<ArticleRating articleId={id}/>
+					<ArticleRecommendationsList/>
 					<ArticleDetailsComments id={id}/>
 				</VStack>
 			</Page>
