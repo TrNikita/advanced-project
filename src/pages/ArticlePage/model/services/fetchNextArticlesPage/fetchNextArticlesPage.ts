e5ab90de17e263847bbe0127ee2ef396c3ab16df
Nikita <tr_nikita@mail.ru>
2023-04-21 12:50:43 +0700

    add db articles

--- src/pages/ArticlePage/model/services/fetchNextArticlesPage/fetchNextArticlesPage.ts
+++ src/pages/ArticlePage/model/services/fetchNextArticlesPage/fetchNextArticlesPage.ts
@@ -25,7 +25,7 @@ export const fetchNextArticlesPage =
 
 				if (hasMore && !isLoading) {
 					dispatch(articlePageActions.setPage(page + 1));
-					dispatch(fetchArticlesList({ replace: true }));
+					dispatch(fetchArticlesList({ replace: false }));
 				}
 			}
 		);
