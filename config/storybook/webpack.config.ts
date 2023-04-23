a428a125975d790e9f51878f2fbd361773f7e70c
Nikita <tr_nikita@mail.ru>
2023-04-23 22:58:49 +0700

    fix storybook

--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -17,7 +17,10 @@ export default ({ config }: { config: webpack.Configuration }) => {
 	};
 	config!.resolve!.modules!.push(paths.src);
 	config!.resolve!.extensions!.push('.ts', '.tsx');
-	config!.resolve!.alias = { '@': path.resolve(__dirname, '..', '..', 'src') };
+	config!.resolve!.alias = {
+		...config!.resolve!.alias,
+		'@': paths.src,
+	};
 
 	const rules = config.module!.rules as RuleSetRule[];
 
--- src/features/articleRating/ui/ArticleRating/ArticleRating.stories.tsx
+++ src/features/articleRating/ui/ArticleRating/ArticleRating.stories.tsx
@@ -1,7 +1,10 @@
 import { ComponentMeta, ComponentStory } from '@storybook/react';
 import React from 'react';
+import withMock from 'storybook-addon-mock';
 
-import  ArticleRating  from './ArticleRating';
+import { StoreDecorator } from '@/shared/config/storybook/StoreDecorator/StoreDecorator';
+
+import ArticleRating from './ArticleRating';
 
 
 export default {
@@ -10,9 +13,48 @@ export default {
 	argTypes: {
 		backgroundColor: { control: 'color' },
 	},
+	decorators: [
+		withMock,
+		StoreDecorator({
+			user: {
+				authData: { id: '1' }
+			}
+		})
+	]
 } as ComponentMeta<typeof ArticleRating>;
 
 const Template: ComponentStory<typeof ArticleRating> = (args) => <ArticleRating {...args} />;
 
 export const Normal = Template.bind({});
-Normal.args = {};
+Normal.args = {
+	articleId: '1'
+};
+
+Normal.parameters = {
+	mockData: [
+		{
+			url: `${__API__}/article-ratings?userId=1&articleId=1`,
+			method: 'GET',
+			status: 200,
+			response: [
+				{
+					rate: 4
+				}
+			],
+		}]
+};
+
+export const WithoutRate = Template.bind({});
+WithoutRate.args = {
+	articleId: '1'
+};
+
+WithoutRate.parameters = {
+	mockData: [
+		{
+			url: `${__API__}/article-ratings?userId=1&articleId=1`,
+			method: 'GET',
+			status: 200,
+			response: [],
+		}]
+};
