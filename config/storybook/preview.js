efc0db854a3cb33b495cb348758a01693bf859c5
Nikita <tr_nikita@mail.ru>
2023-04-26 16:49:29 +0700

    update screens

--- config/storybook/preview.js
+++ config/storybook/preview.js
@@ -4,6 +4,7 @@ import { RouterDecorator } from '../../src/shared/config/storybook/RouterDecorat
 import { StyleDecorator } from '../../src/shared/config/storybook/StyleDecorator/StyleDecorator';
 import { SuspenseDecorator } from '../../src/shared/config/storybook/SuspenseDecorator/SuspenseDecorator';
 import { ThemeDecorator } from '../../src/shared/config/storybook/ThemeDecorator/ThemeDecorator';
+import { Theme } from '../../src/shared/const/theme';
 
 
 export const parameters = {
@@ -14,6 +15,7 @@ export const parameters = {
 			date: /Date$/,
 		},
 	},
+	layout: 'fullscreen'
 };
 
 addDecorator(StyleDecorator);
