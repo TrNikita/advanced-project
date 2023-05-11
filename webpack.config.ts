ed10e06fb528a94491e0eaedafd45c5d448196ff
Nikita <tr_nikita@mail.ru>
2023-05-11 23:51:57 +0700

    add get api url function

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,7 +1,18 @@
 import path from 'path';
 import webpack from 'webpack';
 import { buildWebpackConfig } from './config/build/buildWebpackConfig';
-import { BuildEnv, BuildPaths } from './config/build/types/config';
+import { BuildEnv, BuildMode, BuildPaths } from './config/build/types/config';
+
+function getApiUrl(mode: BuildMode, apiUrl?: string) {
+	if (apiUrl) {
+		return apiUrl;
+	}
+	if (mode === 'production') {
+		return '/api';
+	}
+
+	return 'http://localhost:8000';
+}
 
 export default (env: BuildEnv) => {
 	const paths: BuildPaths = {
