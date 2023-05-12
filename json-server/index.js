c3b19042c5a874e3f48fafd829e0842444744598
Nikita <tr_nikita@mail.ru>
2023-05-13 01:30:45 +0700

    fix proxy to nginx.conf

--- json-server/index.js
+++ json-server/index.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const http = require('http');
 const https = require('https');
 const path = require('path');
 const jsonServer = require('json-server');
@@ -61,7 +62,15 @@ server.use(router);
 
 // запуск сервера
 const PORT = 8443;
+const HTTP_PORT = 8000;
+
 const httpsServer = https.createServer(options, server);
+const httpServer = http.createServer(server);
+
 httpsServer.listen(PORT, () => {
-	console.log('server is running on 8000 port');
+	console.log(`server is running on ${PORT} port`);
+});
+
+httpServer.listen(HTTP_PORT, () => {
+	console.log(`server is running on ${HTTP_PORT} port`);
 });
