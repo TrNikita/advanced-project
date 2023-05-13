f4c64c5674e13718e1fc5e577cb55896bcab42b3
Nikita <tr_nikita@mail.ru>
2023-05-13 18:47:13 +0700

    add feature flags concept

--- src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.tsx
+++ src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.tsx
@@ -1,6 +1,7 @@
 import { memo } from 'react';
 import { useParams } from 'react-router-dom';
 import { ArticleDetails } from '@/entities/Article';
+import { Counter } from '@/entities/Counter';
 import { ArticleRating } from '@/features/articleRating';
 import { ArticleRecommendationsList } from '@/features/articleRecommendationsList';
 import { classNames } from '@/shared/lib/classNames/classNames';
@@ -8,6 +9,7 @@ import {
 	DynamicModuleLoader,
 	ReducersList,
 } from '@/shared/lib/components/DynamicModuleLoader/DynamicModuleLoader';
+import { getFeatureFlag } from '@/shared/lib/features';
 import { VStack } from '@/shared/ui/Stack';
 import { Page } from '@/widgets/Page';
 import cls from './ArticleDetailsPage.module.scss';
@@ -26,6 +28,8 @@ const reducers: ReducersList = {
 const ArticleDetailsPage = (props: ArticleDetailsPageProps) => {
 	const { className } = props;
 	const { id } = useParams<{ id: string }>();
+	const isArticleRatingEnabled = getFeatureFlag('isArticleRatingEnabled');
+	const isCounterEnabled = getFeatureFlag('isCounterEnabled');
 
 	if (!id) {
 		return null;
@@ -39,7 +43,8 @@ const ArticleDetailsPage = (props: ArticleDetailsPageProps) => {
 				<VStack gap="16" max>
 					<ArticleDetailsPageHeader />
 					<ArticleDetails id={id} />
-					<ArticleRating articleId={id} />
+					{isCounterEnabled && <Counter />}
+					{isArticleRatingEnabled && <ArticleRating articleId={id} />}
 					<ArticleRecommendationsList />
 					<ArticleDetailsComments id={id} />
 				</VStack>
