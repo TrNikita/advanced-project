889e0c78c7369d026294cd6e8cbc9aaca9a48a97
Nikita <tr_nikita@mail.ru>
2023-05-16 23:57:19 +0700

    redesigned sidebar

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -2,11 +2,13 @@ import { memo, useMemo, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { LangSwitcher } from '@/features/LangSwitcher';
 import { ThemeSwitcher } from '@/features/ThemeSwitcher';
+import ArrowIcon from '@/shared/assets/icons/arrow-bottom.svg';
 import { classNames } from '@/shared/lib/classNames/classNames';
 import { ToggleFeatures } from '@/shared/lib/features';
-import { AppLogo } from '@/shared/ui/deprecated/AppLogo';
 import { Button, ButtonSize, ButtonTheme } from '@/shared/ui/deprecated/Button';
 import { VStack } from '@/shared/ui/deprecated/Stack';
+import { AppLogo } from '@/shared/ui/redesigned/AppLogo';
+import { Icon } from '@/shared/ui/redesigned/Icon';
 import cls from './Sidebar.module.scss';
 import { getSidebarItems } from '../../model/selectors/getSidebarItems';
 import { SidebarItem } from '../SidebarItem/SidebarItem';
@@ -49,7 +51,20 @@ export const Sidebar = memo(({ className }: SidebarProps) => {
 						[className],
 					)}
 				>
-					<AppLogo className={cls.appLogo} />
+					<AppLogo
+						size={collapsed ? 30 : 50}
+						className={cls.appLogo}
+					/>
+					<VStack role="navigation" gap="8" className={cls.items}>
+						{itemsList}
+					</VStack>
+					<Icon
+						data-testid="sidebar-toggle"
+						onClick={onToggle}
+						className={cls.collapsedBtn}
+						Svg={ArrowIcon}
+					>
+					</Icon>
 				</aside>
 			}
 			off={
@@ -71,9 +86,6 @@ export const Sidebar = memo(({ className }: SidebarProps) => {
 					>
 						{collapsed ? '>' : '<'}
 					</Button>
-					<VStack role="navigation" gap="8" className={cls.items}>
-						{itemsList}
-					</VStack>
 					<div className={cls.switchers}>
 						<ThemeSwitcher />
 						<LangSwitcher className={cls.lang} short={collapsed} />
