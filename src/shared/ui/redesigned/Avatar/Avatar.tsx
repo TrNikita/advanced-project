b0c14fe97cc822c8716ed0edadb9e4724fb666d2
Nikita <tr_nikita@mail.ru>
2023-05-19 00:35:34 +0700

    redesigned Avatar module

--- src/shared/ui/redesigned/Avatar/Avatar.tsx
+++ src/shared/ui/redesigned/Avatar/Avatar.tsx
@@ -11,15 +11,10 @@ interface AvatarProps {
 	src?: string;
 	size?: number;
 	alt?: string;
-	fallbackInverted?: boolean;
 }
 
-/**
- * Устарел, используем новые компоненты из папки redesigned
- * @deprecated
- */
 export const Avatar = (props: AvatarProps) => {
-	const { className, src, size = 100, alt, fallbackInverted } = props;
+	const { className, src, size = 100, alt } = props;
 
 	const styles = useMemo<CSSProperties>(() => {
 		return {
@@ -29,14 +24,7 @@ export const Avatar = (props: AvatarProps) => {
 	}, [size]);
 
 	const fallback = <Skeleton width={size} height={size} border="50%" />;
-	const errorFallback = (
-		<Icon
-			inverted={fallbackInverted}
-			Svg={UserIcon}
-			width={size}
-			height={size}
-		/>
-	);
+	const errorFallback = <Icon Svg={UserIcon} width={size} height={size} />;
 
 	return (
 		<AppImage
