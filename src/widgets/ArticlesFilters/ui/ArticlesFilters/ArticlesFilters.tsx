e9730fd9baab18ceae717ed44277d1006a094874
Nikita <tr_nikita@mail.ru>
2023-05-22 22:48:19 +0700

    add addons for Input

--- src/widgets/ArticlesFilters/ui/ArticlesFilters/ArticlesFilters.tsx
+++ src/widgets/ArticlesFilters/ui/ArticlesFilters/ArticlesFilters.tsx
@@ -3,9 +3,11 @@ import { useTranslation } from 'react-i18next';
 import { ArticleSortField, ArticleType } from '@/entities/Article';
 import { ArticleSortSelector } from '@/features/ArticleSortSelector';
 import { ArticleTypeTabs } from '@/features/ArticleTypeTabs';
+import SearchIcon from '@/shared/assets/icons/search.svg';
 import { classNames } from '@/shared/lib/classNames/classNames';
 import { SortOrder } from '@/shared/types/sort';
 import { Card } from '@/shared/ui/redesigned/Card';
+import { Icon } from '@/shared/ui/redesigned/Icon';
 import { Input } from '@/shared/ui/redesigned/Input';
 import { VStack } from '@/shared/ui/redesigned/Stack';
 import cls from './ArticlesFilters.module.scss';
@@ -46,6 +48,7 @@ export const ArticlesFilters = memo((props: ArticlesFiltersProps) => {
 					onChange={onChangeSearch}
 					value={search}
 					placeholder={t('Поиск')}
+					addonLeft={<Icon Svg={SearchIcon} />}
 				/>
 				<ArticleSortSelector
 					order={order}
