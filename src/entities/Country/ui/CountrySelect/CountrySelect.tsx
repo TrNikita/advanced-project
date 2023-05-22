a0faa0dbb6259aade594d5244730cb155daea586
Nikita <tr_nikita@mail.ru>
2023-05-23 00:38:33 +0700

    redesigned CountrySelect and CurrencySelect

--- src/entities/Country/ui/CountrySelect/CountrySelect.tsx
+++ src/entities/Country/ui/CountrySelect/CountrySelect.tsx
@@ -1,6 +1,8 @@
 import { memo, useCallback } from 'react';
 import { useTranslation } from 'react-i18next';
-import { ListBox } from '@/shared/ui/deprecated/Popups';
+import { ToggleFeatures } from '@/shared/lib/features';
+import { ListBox as ListBoxDeprecated } from '@/shared/ui/deprecated/Popups';
+import { ListBox } from '@/shared/ui/redesigned/Popups';
 import { Country } from '../../model/types/country';
 
 interface CountrySelectProps {
@@ -19,7 +21,7 @@ const options = [
 ];
 
 export const CountrySelect = memo(
-	({ value, onChange, readonly }: CountrySelectProps) => {
+	({ className, value, onChange, readonly }: CountrySelectProps) => {
 		const { t } = useTranslation();
 
 		const onChangeHandler = useCallback(
@@ -29,15 +31,22 @@ export const CountrySelect = memo(
 			[onChange],
 		);
 
+		const props = {
+			className,
+			value,
+			defaultValue: t('Укажите страну'),
+			label: t('Укажите страну'),
+			items: options,
+			onChange: onChangeHandler,
+			readonly,
+			direction: 'top right' as const,
+		};
+
 		return (
-			<ListBox
-				onChange={onChangeHandler}
-				value={value}
-				defaultValue={t('Укажите страну')}
-				label={t('Укажите страну')}
-				items={options}
-				readonly={readonly}
-				direction="top right"
+			<ToggleFeatures
+				feature="isAppRedesigned"
+				on={<ListBox {...props} />}
+				off={<ListBoxDeprecated {...props} />}
 			/>
 		);
 	},
