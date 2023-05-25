847f008903e28872ef8d24273e723a0d87d802ba
Nikita <tr_nikita@mail.ru>
2023-05-26 00:22:12 +0700

    redesigned modal

--- src/shared/ui/redesigned/Modal/Modal.tsx
+++ src/shared/ui/redesigned/Modal/Modal.tsx
@@ -1,10 +1,11 @@
 import { ReactNode } from 'react';
 import { classNames, Mods } from '@/shared/lib/classNames/classNames';
+import { toggleFeatures } from '@/shared/lib/features';
 import { useModal } from '@/shared/lib/hooks/useModal/useModal';
 import { useTheme } from '@/shared/lib/hooks/useTheme/useTheme';
 import cls from './Modal.module.scss';
-import { Overlay } from '../../redesigned/Overlay/Overlay';
-import { Portal } from '../../redesigned/Portal/Portal';
+import { Overlay } from '../Overlay/Overlay';
+import { Portal } from '../Portal/Portal';
 
 interface ModalProps {
 	className?: string;
@@ -41,12 +42,17 @@ export const Modal = (props: ModalProps) => {
 	}
 
 	return (
-		<Portal>
+		<Portal element={document.getElementById('app') ?? document.body}>
 			<div
 				className={classNames(cls.Modal, mods, [
 					className,
 					theme,
 					'app_modal',
+					toggleFeatures({
+						name: 'isAppRedesigned',
+						on: () => cls.modalNew,
+						off: () => cls.modalOld
+					})
 				])}
 			>
 				<Overlay onClick={close} />
