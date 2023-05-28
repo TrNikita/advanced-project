ea09ce1d907c7110b606a2e7c3906366d042faaa
Nikita <tr_nikita@mail.ru>
2023-05-28 23:34:37 +0700

    redesigned comments

--- src/entities/Comment/ui/CommentList/CommentList.tsx
+++ src/entities/Comment/ui/CommentList/CommentList.tsx
@@ -1,8 +1,10 @@
 import { memo } from 'react';
 import { useTranslation } from 'react-i18next';
 import { classNames } from '@/shared/lib/classNames/classNames';
-import { Text } from '@/shared/ui/deprecated/Text';
+import { ToggleFeatures } from '@/shared/lib/features';
+import { Text as TextDeprecated } from '@/shared/ui/deprecated/Text';
 import { VStack } from '@/shared/ui/redesigned/Stack';
+import { Text } from '@/shared/ui/redesigned/Text';
 import { Comment } from '../../model/types/comment';
 import { CommentCard } from '../CommentCard/CommentCard';
 
@@ -13,12 +15,12 @@ interface CommentListProps {
 }
 
 export const CommentList = memo((props: CommentListProps) => {
-	const { className, comments, isLoading } = props;
+	const { className, isLoading, comments } = props;
 	const { t } = useTranslation();
 
 	if (isLoading) {
 		return (
-			<VStack gap={'16'} max className={classNames('', {}, [className])}>
+			<VStack gap="16" max className={classNames('', {}, [className])}>
 				<CommentCard isLoading />
 				<CommentCard isLoading />
 				<CommentCard isLoading />
@@ -27,7 +29,7 @@ export const CommentList = memo((props: CommentListProps) => {
 	}
 
 	return (
-		<VStack gap="16" max>
+		<VStack gap="16" max className={classNames('', {}, [className])}>
 			{comments?.length ? (
 				comments.map((comment) => (
 					<CommentCard
@@ -37,7 +39,11 @@ export const CommentList = memo((props: CommentListProps) => {
 					/>
 				))
 			) : (
-				<Text text={t('Комментарии отсутствуют')} />
+				<ToggleFeatures
+					feature="isAppRedesigned"
+					on={<Text text={t('Комментарии отсутствуют')} />}
+					off={<TextDeprecated text={t('Комментарии отсутствуют')} />}
+				/>
 			)}
 		</VStack>
 	);
