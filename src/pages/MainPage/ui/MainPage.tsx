e724894c87c460fb6eb0ff3035c3f060e289ebb9
Nikita <tr_nikita@mail.ru>
2023-06-02 23:03:10 +0700

    minor fix

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-import { UiDesignSwitcher } from '@/features/uiDesignSwitcher';
 import { Page } from '@/widgets/Page';
 
 const MainPage = () => {
